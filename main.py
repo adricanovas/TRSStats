703fd745dcfa5b06c96411a474c2192f0e42fc3d
Adri <adrian.canovas1@um.es>
2022-11-26 23:38:10 +0100

    Window structure

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import sys
 
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QPushButton, QWidget, QVBoxLayout, QGridLayout, \
+    QLineEdit, QFormLayout, QToolBar, QStatusBar, QLabel
 
-class MainWindow(QMainWindow):
+
+class Window(QMainWindow):
     def __init__(self):
-        super().__init__()
-
+        super().__init__(parent=None)
         self.setWindowTitle("TRS Stats")
         self.setFixedSize(QSize(500, 800))
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
 
-app = QApplication(sys.argv)
+        def _createMenu(self):
+            menu = self.menuBar().addMenu("&Menu")
+            menu.addAction("&Exit", self.close)
 
-window = MainWindow()
-window.show()
-app.exec()
+        def _createToolBar(self):
+            tools = QToolBar()
+            tools.addAction("Exit", self.close)
+            self.addToolBar(tools)
+
+        def _createStatusBar(self):
+            status = QStatusBar()
+            status.showMessage("I'm the Status Bar")
+            self.setStatusBar(status)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
 
